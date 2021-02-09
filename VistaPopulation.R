a985916b243c98ec94b933ddbcd276b67b25c954
jafshin <afshin.jafari@rmit.edu.au>
2021-02-10 10:19:22 +1100

    adding weights, histogram and merging 65+

--- VistaPopulation.R
+++ VistaPopulation.R
@@ -3,6 +3,7 @@
 
 library(dplyr)
 library(readr)
+library(ggplot2)
 
 # Reading inputs ----------------------------------------------------------
 
@@ -14,6 +15,10 @@ trips <- read_csv("./input/T_VISTA1218_V1.csv") # VISTA 2012-18 trips
 # Only included trip purposes that are meaningful for comparison
 # i.e. everyone goes home, so it won't make sense to compare demographics for it
 tripsPorcessed <- trips %>% 
+  # Using both weekend and weekday weights as it seems to be what demand algorithm
+  # is using, we might want to change it later
+  mutate(weight                  =ifelse(is.na(WDTRIPWGT),
+                                         WETRIPWGT, WDTRIPWGT)) %>% 
   mutate(Work                    =ifelse(ORIGPURP1=="Work Related",
                                          yes=1, no=0)) %>% 
   mutate(Study                   =ifelse(ORIGPURP1=="Education",
@@ -30,18 +35,20 @@ tripsPorcessed <- trips %>%
   #mutate('Mode Change' = ifelse(ORIGPURP1=="Change Mode",yes = 1,no = 0)) %>% 
   #mutate('With Someone' = ifelse(ORIGPURP1=="Accompany Someone",yes = 1,no = 0)) 
   dplyr::select(PERSID, LINKMODE, ORIGPURP1, Work, Study, Shop, Personal, 
-                'Social/Recreational', 'Pickup/Dropoff/Deliver')
+                'Social/Recreational', 'Pickup/Dropoff/Deliver', weight)
 
-# Taking the max because we only care if the person had a trip of a given type
-# or not. Change it to sum if you care "how many" about number of each trip type
+# Taking the sum because we care about ratios based on trip types
 tripsGrouped <- tripsPorcessed %>% 
   group_by(PERSID) %>% 
   summarise(NUMTRIPS=n(), 
-            Work=max(Work), Study=max(Study),
-            Shop=max(Shop), Personal=max(Personal),
-            `Social/Recreational`=max(`Social/Recreational`), 
-            `Pickup/Dropoff/Deliver`=max(`Pickup/Dropoff/Deliver`))
+            Work=sum(Work*weight), Study=sum(Study*weight),
+            Shop=sum(Shop*weight), Personal=sum(Personal*weight),
+            `Social/Recreational`=sum(`Social/Recreational`*weight), 
+            `Pickup/Dropoff/Deliver`=sum(`Pickup/Dropoff/Deliver`*weight),
+            weight=mean(weight))
 
+# Age group 0-15: Children, 15-40: Working Age Category 1, 
+# 40-65: Working Age Category 2, 65+: Over 65
 personsJoined <- persons %>% 
   dplyr::select(PERSID,AGE, SEX, ANYWORK) %>% 
   left_join(tripsGrouped, by="PERSID") %>% 
@@ -49,12 +56,20 @@ personsJoined <- persons %>%
   mutate(AGEGroup=ifelse(AGE<15          ,"CHILDREN"   , "NA")) %>% 
   mutate(AGEGroup=ifelse(AGE<40 & 14<AGE ,"WORINGAGE1" , AGEGroup)) %>% 
   mutate(AGEGroup=ifelse(AGE<65 & 39<AGE ,"WORINGAGE2" , AGEGroup)) %>% 
-  mutate(AGEGroup=ifelse(AGE<85 & 64<AGE ,"OVER65"     , AGEGroup)) %>% 
-  mutate(AGEGroup=ifelse(         84<AGE ,"OVER85"     , AGEGroup)) 
+  mutate(AGEGroup=ifelse(         64<AGE ,"OVER65"     , AGEGroup)) 
+
+# Plot trip lenght total
+plot <- personsJoined %>% 
+  filter(NUMTRIPS>0) %>% 
+  slice(rep(1:n(), each = round(weight))) %>%  # repeating rows based on the weights
+  ggplot(aes(x=NUMTRIPS)) +
+  geom_histogram(binwidth = 1)
+
+ggsave("tripLength.pdf")
 
 TripProbablities <- personsJoined %>% 
   group_by(SEX,AGEGroup, ANYWORK) %>% 
-  summarise(TripLength= mean(NUMTRIPS),
+  summarise(TripLength= mean(NUMTRIPS), TripLenghtSD=sd(NUMTRIPS),
             Work=sum(Work), Study=sum(Study),
             Shop=sum(Shop), Personal=sum(Personal),
             `Social/Recreational`=sum(`Social/Recreational`), 
@@ -67,4 +82,4 @@ TripProbablities <- personsJoined %>%
          `Social/Recreational`=`Social/Recreational`/sum(`Social/Recreational`),
          `Pickup/Dropoff/Deliver`=`Pickup/Dropoff/Deliver`/sum(`Pickup/Dropoff/Deliver`))  
 
-write_csv(TripProbablities, "TripProbablities.csv")
+write_csv(TripProbablities, "WeightedTripProbablities.csv")
